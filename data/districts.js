2176ca041eb81ecc38b2c3bd2d89d83b7caa200c
dariansweb <dariansweb@gmail.com>
2025-03-30 19:05:59 -0500

    provider file is completed but still needs counties

---
+++ data/districts.js
@@ -0,0 +1,130 @@
+export const districtData = {
+  "1": [
+    "Cross",
+    "Woodruff",
+    "St. Francis",
+    "Monroe",
+    "Lee",
+    "Phillips"
+  ],
+  "2": [
+    "Crittenden",
+    "Mississippi",
+    "Poinsett",
+    "Craighead",
+    "Greene",
+    "Clay"
+  ],
+  "3": [
+    "Jackson",
+    "Lawrence",
+    "Sharp",
+    "Randolph"
+  ],
+  "4": [
+    "Washington", 
+    "Madison"
+  ],
+  "5": [
+    "Franklin",
+    "Johnson", 
+    "Pope"
+  ],
+  "6": [
+    "Pulaski",
+    "Perry"
+  ],
+  "7": [
+    "Hot Spring",
+    "Grant"
+  ],
+  "8N": [
+    "Hempstead",
+    "Nevada"
+  ],
+  "8S": [
+    "Miller",
+    "Lafayette"
+  ],
+  "9W": [
+    "Little River",
+    "Sevier",
+    "Howard",
+    "Pike"
+  ],
+  "9E": [
+    "Clark"
+  ],
+  "10": [
+    "Ashley",
+    "Bradley",
+    "Drew",
+    "Desha"
+  ],
+  "11E": [
+    "Arkansas"
+  ],
+  "11W": [
+    "Jefferson",
+    "Lincoln"
+  ],
+  "12": [
+    "Sebastian"
+  ],
+  "13": [
+    "Union",
+    "Columbia",
+    "Ouachita",
+    "Calhoun",
+    "Cleveland",
+    "Dallas"
+  ],
+  "14": [
+    "Boone",
+    "Marion",
+    "Baxter",
+    "Newton"
+  ],
+  "15": [
+    "Scott",
+    "Logan",
+    "Yell",
+    "Conway"
+  ],
+  "16": [
+    "Cleburne",
+    "Independence",
+    "Stone",
+    "Izard",
+    "Fulton"
+  ],
+  "17": [
+    "Prairie",
+    "White"
+  ],
+  "18E": [
+    "Garland"
+  ],
+  "18W": [
+    "Polk",
+    "Montgomery"
+  ],
+  "19W": [
+    "Benton",
+    "Carroll"
+  ], 
+  "20": [
+    "Searcy", 
+    "Van Buren",
+    "Faulkner"
+  ],
+  "21": [
+    "Crawford"
+  ],
+  "22": [
+    "Saline"
+  ],
+  "23": [
+    "Lonoke"
+  ]
+};
